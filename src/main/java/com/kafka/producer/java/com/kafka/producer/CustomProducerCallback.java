8ed4624c99c5d330cb683765c38edfd3277e88e5
gaogao <gaoyanshuo9388@yahoo.com>
2023-03-20 12:02:40 +0900

    add producer async send function

--- src/main/java/com/kafka/producer/java/com/kafka/producer/CustomProducerCallback.java
+++ src/main/java/com/kafka/producer/java/com/kafka/producer/CustomProducerCallback.java
@@ -1,17 +1,15 @@
 package com.kafka.producer.java.com.kafka.producer;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 
 import java.util.Properties;
 
-public class CustomProducer {
+public class CustomProducerCallback {
     public static void main(String[] args) {
         //0. 配置
         Properties properties = new Properties();
         //连接集群
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "8.210.69.42:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "public:9092");
         //指定对应的key和value序列化
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
@@ -19,7 +17,17 @@ public class CustomProducer {
         //1. 创建 kafka 生产者的配置对象
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
         //2.send
-        kafkaProducer.send(new ProducerRecord<>("firstTopic", "gaogaoNB!!!"));
+        for (int i = 0; i < 3; i++) {
+            kafkaProducer.send(new ProducerRecord<>("firstTopic", "this is " + i + "th value"), new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        System.out.println("topic: " + recordMetadata.topic() + " partition: " + recordMetadata.partition());
+                    }
+                }
+            });
+        }
+
         //3.close
         kafkaProducer.close();
     }
