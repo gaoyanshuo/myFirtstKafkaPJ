2a804c09c76e8a09535f33348fe4d9c7b14bc988
gaogao <gaoyanshuo9388@yahoo.com>
2023-03-20 12:10:32 +0900

    add producer sync function

--- src/main/java/com/kafka/producer/java/com/kafka/producer/CustomProducerCallbackSync.java
+++ src/main/java/com/kafka/producer/java/com/kafka/producer/CustomProducerCallbackSync.java
@@ -3,13 +3,14 @@ package com.kafka.producer.java.com.kafka.producer;
 import org.apache.kafka.clients.producer.*;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
-public class CustomProducerCallback {
-    public static void main(String[] args) {
+public class CustomProducerCallbackSync {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //0. 配置
         Properties properties = new Properties();
         //连接集群
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "8.210.69.42:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "public:9092");
         //指定对应的key和value序列化
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
@@ -18,14 +19,7 @@ public class CustomProducerCallback {
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
         //2.send
         for (int i = 0; i < 3; i++) {
-            kafkaProducer.send(new ProducerRecord<>("firstTopic", "this is " + i + "th value"), new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    if (e == null) {
-                        System.out.println("topic: " + recordMetadata.topic() + " partition: " + recordMetadata.partition());
-                    }
-                }
-            });
+            kafkaProducer.send(new ProducerRecord<>("firstTopic", "this is " + i + "th value")).get();
         }
 
         //3.close
